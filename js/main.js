8e3b10b6835f0de9ec940265b96f04ccab4eea34
Brit Butler <redline6561@gmail.com>
2016-06-21 12:00:11 -0400

    Add functions to handle the various button clicks.

--- js/main.js
+++ js/main.js
@@ -16,4 +16,47 @@ var processButton = function (event) {
   if (dataType === "equal") return processEqual(target);
 };
 
+var updateDisplay = function () {
+  var display = $("#display");
+  return display.html(calcState.left + " " + calcState.operator + " " + calcState.right);
+};
+
+var processNumber = function (button) {
+  if (calcState.operator === "") {
+    calcState.left += button.textContent;
+  } else {
+    calcState.right += button.textContent;
+  }
+  return updateDisplay();
+};
+
+var processOperator = function (button) {
+  calcState.operator = button.textContent;
+  return updateDisplay();
+};
+
+var processClear = function (button) {
+  calcState = { left: "", operator: "", right: "" };
+  updateDisplay();
+};
+
+var processEqual = function (button) {
+  var op = calcState.operator;
+  var left = Number(calcState.left);
+  var right = Number(calcState.right);
+  if (op === "" || calcState.left === "" || calcState.right === "") {
+    return alert("Error: You need a left term, right term, and operator.");
+  }
+  if (op === "+") {
+    calcState = { left: left + right, operator: "", right: "" };
+  } else if (op === "-") {
+    calcState = { left: left - right, operator: "", right: "" };
+  } else if (op === "*") {
+    calcState = { left: left * right, operator: "", right: "" };
+  } else if (op === "/") {
+    calcState = { left: left / right, operator: "", right: "" };
+  }
+  return updateDisplay();
+};
+
 $(".buttons").on('click', processButton);
